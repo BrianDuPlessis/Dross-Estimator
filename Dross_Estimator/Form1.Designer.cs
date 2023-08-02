1f740315249d90a93f0be559cf71451e86af1028
BrianDuPlessis <112475285+BrianDuPlessis@users.noreply.github.com>
2023-08-02 13:08:30 +0200

    Added project files
    The program files were added

---
+++ Dross_Estimator/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace Dross_Estimator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlZincKettle = new System.Windows.Forms.Panel();
+            this.txtMid4 = new System.Windows.Forms.TextBox();
+            this.txtMid3 = new System.Windows.Forms.TextBox();
+            this.txtMid2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMid1 = new System.Windows.Forms.TextBox();
+            this.txtMid5 = new System.Windows.Forms.TextBox();
+            this.txtRight1 = new System.Windows.Forms.TextBox();
+            this.txtRight2 = new System.Windows.Forms.TextBox();
+            this.txtRight3 = new System.Windows.Forms.TextBox();
+            this.txtRight4 = new System.Windows.Forms.TextBox();
+            this.txtRight5 = new System.Windows.Forms.TextBox();
+            this.txtLeft5 = new System.Windows.Forms.TextBox();
+            this.txtLeft4 = new System.Windows.Forms.TextBox();
+            this.txtLeft3 = new System.Windows.Forms.TextBox();
+            this.txtLeft2 = new System.Windows.Forms.TextBox();
+            this.txtLeft1 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.pnlZincKettle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(416, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zinc Kettle dross level";
+            // 
+            // pnlZincKettle
+            // 
+            this.pnlZincKettle.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pnlZincKettle.Controls.Add(this.txtMid4);
+            this.pnlZincKettle.Controls.Add(this.txtMid3);
+            this.pnlZincKettle.Controls.Add(this.txtMid2);
+            this.pnlZincKettle.Controls.Add(this.label3);
+            this.pnlZincKettle.Controls.Add(this.label2);
+            this.pnlZincKettle.Location = new System.Drawing.Point(398, 168);
+            this.pnlZincKettle.Name = "pnlZincKettle";
+            this.pnlZincKettle.Size = new System.Drawing.Size(318, 485);
+            this.pnlZincKettle.TabIndex = 5;
+            // 
+            // txtMid4
+            // 
+            this.txtMid4.Location = new System.Drawing.Point(94, 396);
+            this.txtMid4.Name = "txtMid4";
+            this.txtMid4.Size = new System.Drawing.Size(134, 22);
+            this.txtMid4.TabIndex = 8;
+            // 
+            // txtMid3
+            // 
+            this.txtMid3.Location = new System.Drawing.Point(94, 232);
+            this.txtMid3.Name = "txtMid3";
+            this.txtMid3.Size = new System.Drawing.Size(134, 22);
+            this.txtMid3.TabIndex = 7;
+            // 
+            // txtMid2
+            // 
+            this.txtMid2.Location = new System.Drawing.Point(94, 99);
+            this.txtMid2.Name = "txtMid2";
+            this.txtMid2.Size = new System.Drawing.Size(134, 22);
+            this.txtMid2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(71, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Entry";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(71, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Skid";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMid1
+            // 
+            this.txtMid1.Location = new System.Drawing.Point(492, 123);
+            this.txtMid1.Name = "txtMid1";
+            this.txtMid1.Size = new System.Drawing.Size(134, 22);
+            this.txtMid1.TabIndex = 5;
+            // 
+            // txtMid5
+            // 
+            this.txtMid5.Location = new System.Drawing.Point(492, 677);
+            this.txtMid5.Name = "txtMid5";
+            this.txtMid5.Size = new System.Drawing.Size(134, 22);
+            this.txtMid5.TabIndex = 9;
+            // 
+            // txtRight1
+            // 
+            this.txtRight1.Location = new System.Drawing.Point(796, 201);
+            this.txtRight1.Name = "txtRight1";
+            this.txtRight1.Size = new System.Drawing.Size(134, 22);
+            this.txtRight1.TabIndex = 10;
+            // 
+            // txtRight2
+            // 
+            this.txtRight2.Location = new System.Drawing.Point(796, 279);
+            this.txtRight2.Name = "txtRight2";
+            this.txtRight2.Size = new System.Drawing.Size(134, 22);
+            this.txtRight2.TabIndex = 11;
+            // 
+            // txtRight3
+            // 
+            this.txtRight3.Location = new System.Drawing.Point(796, 400);
+            this.txtRight3.Name = "txtRight3";
+            this.txtRight3.Size = new System.Drawing.Size(134, 22);
+            this.txtRight3.TabIndex = 12;
+            // 
+            // txtRight4
+            // 
+            this.txtRight4.Location = new System.Drawing.Point(796, 527);
+            this.txtRight4.Name = "txtRight4";
+            this.txtRight4.Size = new System.Drawing.Size(134, 22);
+            this.txtRight4.TabIndex = 13;
+            // 
+            // txtRight5
+            // 
+            this.txtRight5.Location = new System.Drawing.Point(796, 598);
+            this.txtRight5.Name = "txtRight5";
+            this.txtRight5.Size = new System.Drawing.Size(134, 22);
+            this.txtRight5.TabIndex = 14;
+            // 
+            // txtLeft5
+            // 
+            this.txtLeft5.Location = new System.Drawing.Point(187, 598);
+            this.txtLeft5.Name = "txtLeft5";
+            this.txtLeft5.Size = new System.Drawing.Size(134, 22);
+            this.txtLeft5.TabIndex = 4;
+            // 
+            // txtLeft4
+            // 
+            this.txtLeft4.Location = new System.Drawing.Point(187, 527);
+            this.txtLeft4.Name = "txtLeft4";
+            this.txtLeft4.Size = new System.Drawing.Size(134, 22);
+            this.txtLeft4.TabIndex = 3;
+            // 
+            // txtLeft3
+            // 
+            this.txtLeft3.Location = new System.Drawing.Point(187, 400);
+            this.txtLeft3.Name = "txtLeft3";
+            this.txtLeft3.Size = new System.Drawing.Size(134, 22);
+            this.txtLeft3.TabIndex = 2;
+            // 
+            // txtLeft2
+            // 
+            this.txtLeft2.Location = new System.Drawing.Point(187, 279);
+            this.txtLeft2.Name = "txtLeft2";
+            this.txtLeft2.Size = new System.Drawing.Size(134, 22);
+            this.txtLeft2.TabIndex = 1;
+            // 
+            // txtLeft1
+            // 
+            this.txtLeft1.Location = new System.Drawing.Point(187, 201);
+            this.txtLeft1.Name = "txtLeft1";
+            this.txtLeft1.Size = new System.Drawing.Size(134, 22);
+            this.txtLeft1.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(478, 728);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(159, 35);
+            this.btnCalculate.TabIndex = 15;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(990, 802);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(83, 35);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(796, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 30);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Right";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(187, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 30);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Left";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(478, 769);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(159, 35);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 802);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(83, 35);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(893, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 18);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Date :";
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(896, 49);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(137, 22);
+            this.txtDate.TabIndex = 23;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1085, 838);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtLeft5);
+            this.Controls.Add(this.txtLeft4);
+            this.Controls.Add(this.txtLeft3);
+            this.Controls.Add(this.txtLeft2);
+            this.Controls.Add(this.txtLeft1);
+            this.Controls.Add(this.txtRight5);
+            this.Controls.Add(this.txtRight4);
+            this.Controls.Add(this.txtRight3);
+            this.Controls.Add(this.txtRight2);
+            this.Controls.Add(this.txtRight1);
+            this.Controls.Add(this.txtMid5);
+            this.Controls.Add(this.txtMid1);
+            this.Controls.Add(this.pnlZincKettle);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Dross Estimator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnlZincKettle.ResumeLayout(false);
+            this.pnlZincKettle.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlZincKettle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMid4;
+        private System.Windows.Forms.TextBox txtMid3;
+        private System.Windows.Forms.TextBox txtMid2;
+        private System.Windows.Forms.TextBox txtMid1;
+        private System.Windows.Forms.TextBox txtMid5;
+        private System.Windows.Forms.TextBox txtRight1;
+        private System.Windows.Forms.TextBox txtRight2;
+        private System.Windows.Forms.TextBox txtRight3;
+        private System.Windows.Forms.TextBox txtRight4;
+        private System.Windows.Forms.TextBox txtRight5;
+        private System.Windows.Forms.TextBox txtLeft5;
+        private System.Windows.Forms.TextBox txtLeft4;
+        private System.Windows.Forms.TextBox txtLeft3;
+        private System.Windows.Forms.TextBox txtLeft2;
+        private System.Windows.Forms.TextBox txtLeft1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDate;
+    }
+}
+
---
+++ Dross_Estimator/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dross_Estimator
+{
+    public partial class frmMain : Form
+    {
+
+        const double ZincWeight = 7133;
+        double[] arrHeights = new double[15];
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnSave.Enabled = false;
+            btnClear.Enabled = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ValidateInput()
+        {
+            try
+            {
+
+                //Left validation
+                if (double.TryParse(txtLeft1.Text, out arrHeights[0]))
+                {
+                    if (double.TryParse(txtLeft2.Text, out arrHeights[1]))
+                    {
+                        if (double.TryParse(txtLeft3.Text, out arrHeights[2]))
+                        {
+                            if (double.TryParse(txtLeft4.Text, out arrHeights[3]))
+                            {
+                                if (double.TryParse(txtLeft5.Text, out arrHeights[4]))
+                                {
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Please enter a valid value");
+                                    txtLeft5.Focus();
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Please enter a valid value");
+                                txtLeft4.Focus();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please enter a valid value");
+                            txtLeft3.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid value");
+                        txtLeft2.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid value");
+                    txtLeft1.Focus();
+                }
+
+
+                //mid validation
+                if (double.TryParse(txtMid1.Text, out arrHeights[5]))
+                {
+                    if (double.TryParse(txtMid2.Text, out arrHeights[6]))
+                    {
+                        if (double.TryParse(txtMid3.Text, out arrHeights[7]))
+                        {
+                            if (double.TryParse(txtMid4.Text, out arrHeights[8]))
+                            {
+                                if (double.TryParse(txtMid5.Text, out arrHeights[9]))
+                                {
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Please enter a valid value");
+                                    txtMid5.Focus();
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Please enter a valid value");
+                                txtMid4.Focus();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please enter a valid value");
+                            txtMid3.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid value");
+                        txtMid2.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid value");
+                    txtMid1.Focus();
+                }
+
+                //Right validation
+                if (double.TryParse(txtRight1.Text, out arrHeights[10]))
+                {
+                    if (double.TryParse(txtRight2.Text, out arrHeights[11]))
+                    {
+                        if (double.TryParse(txtRight3.Text, out arrHeights[12]))
+                        {
+                            if (double.TryParse(txtRight4.Text, out arrHeights[13]))
+                            {
+                                if (double.TryParse(txtRight5.Text, out arrHeights[14]))
+                                {
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Please enter a valid value");
+                                    txtRight5.Focus();
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Please enter a valid value");
+                                txtRight4.Focus();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please enter a valid value");
+                            txtRight3.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid value");
+                        txtRight2.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid value");
+                    txtRight1.Focus();
+                }
+
+                if(txtDate.Text == "")
+                {
+                    MessageBox.Show("Please enter the Date");
+                    txtDate.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private double CalculateDross()
+        {
+           double drossVolume = 0;
+            double drossWeight = 0;
+
+            for(int k = 0; k < 15; k++)
+            {
+                drossVolume = drossVolume + ((arrHeights[k] / 1000) * 0.65);
+            }
+            drossWeight = drossVolume * ZincWeight;
+
+            return drossWeight;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double drossWeight;
+
+            ValidateInput();
+            drossWeight = CalculateDross();
+
+            MessageBox.Show("The total estimated dross is " + drossWeight.ToString("f2") + "kg");
+            btnSave.Enabled = true;
+            btnClear.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            btnClear.Enabled = false;
+            txtDate.Clear();
+            //Left
+            txtLeft1.Text = "";
+            txtLeft2.Text = "";
+            txtLeft3.Text = "";
+            txtLeft4.Text = "";
+            txtLeft5.Text = "";
+
+            //Mid
+            txtMid1.Text = "";
+            txtMid2.Text = "";
+            txtMid3.Text = "";
+            txtMid4.Text = "";
+            txtMid5.Text = "";
+
+            //Right
+            txtRight1.Text = "";
+            txtRight2.Text = "";
+            txtRight3.Text = "";
+            txtRight4.Text = "";
+            txtRight5.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string path = "", sOutLeft = "", sOutMid = "", sOutRight = "";
+            double drossWeight;
+            StreamWriter outFile;
+
+            try
+            {
+                drossWeight = CalculateDross();
+                if(saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    path = saveFile.FileName;
+                    outFile = File.AppendText(path);
+
+                    outFile.WriteLine("");
+                    outFile.WriteLine("");
+
+                    //Build string containing left values
+                    for(int k = 0; k < 5; k++)
+                    {
+                        sOutLeft += arrHeights[k].ToString() + " ";
+                    }
+
+                    //Build string containing middel values
+                    for (int k = 5; k < 10; k++)
+                    {
+                        sOutMid += arrHeights[k].ToString() + " ";
+                    }
+
+                    //Build string containing right values
+                    for (int k = 10; k < 15; k++)
+                    {
+                        sOutRight += arrHeights[k].ToString() + " ";
+                    }
+
+                    outFile.WriteLine(txtDate.Text);
+                    outFile.WriteLine("Left values = " + sOutLeft);
+                    outFile.WriteLine("Middel values = " + sOutMid);
+                    outFile.WriteLine("Right values = " + sOutRight);
+                    outFile.WriteLine("Total dross weigth = " + drossWeight.ToString("f2") + "kg.");
+
+                    outFile.Close();
+                }
+            }
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
